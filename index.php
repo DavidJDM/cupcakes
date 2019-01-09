b6f7be399d435d17880463a211706264dd72fe67
David Kovalevich <kovalevichdavid@yahoo.com>
2019-01-08 19:30:56 -0500

    Cupcake check boxes with submit button

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
  * Time: 3:33 PM
  */
 
+$cupcakeFlavors = array("grasshopper" => "The Grasshopper", "maple" => "Whiskey Maple Bacon", "carrot" => "Carrot Walnut", "caramel" => "Salted Caramel Cupcake", "velvet" => "Red Velvet", "lemon" => "Lemon Drop", "tiramisu" => "Tiramisu");
+
 ?>
 
 
@@ -18,8 +20,15 @@
 <body>
 <h1>Order Cupcakes</h1>
 <form method="post" action="">
-    <h4>Enter your name:</h4>
-    <input type="text" name="username" id="username" placeholder="Enter your name here" value="<?php echo $_POST['username']; ?>" required>
+    Enter your name: <input type="text" name="username" id="username" placeholder="Enter your name here" value="<?php echo $_POST['username']; ?>" required><br><br>
+    <input type="checkbox" name="cupcakeFlavors[]" id="grasshopper">&nbsp;<?php print_r($cupcakeFlavors["grasshopper"])?><br>
+    <input type="checkbox" name="cupcakeFlavors[]" id="maple">&nbsp;<?php print_r($cupcakeFlavors["maple"])?><br>
+    <input type="checkbox" name="cupcakeFlavors[]" id="carrot">&nbsp;<?php print_r($cupcakeFlavors["carrot"])?><br>
+    <input type="checkbox" name="cupcakeFlavors[]" id="caramel">&nbsp;<?php print_r($cupcakeFlavors["caramel"])?><br>
+    <input type="checkbox" name="cupcakeFlavors[]" id="velvet">&nbsp;<?php print_r($cupcakeFlavors["velvet"])?><br>
+    <input type="checkbox" name="cupcakeFlavors[]" id="lemon">&nbsp;<?php print_r($cupcakeFlavors["lemon"])?><br>
+    <input type="checkbox" name="cupcakeFlavors[]" id="tiramisu">&nbsp;<?php print_r($cupcakeFlavors["tiramisu"])?><br><br><br>
+    <input type="submit" name="submit" value="Submit Order" id="order">
 </form>
 </body>
 </html>
